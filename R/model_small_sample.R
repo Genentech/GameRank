2e0d929fc86dbc52e14492eec39bf0b3df4bac9d
Henneges, Carsten {MDBA~South San Francisco} <henneges.carsten@gene.com>
2021-11-04 12:09:55 +0100

    Adding functions for small sample variable selection and a stab to the first GameRank vignette, ready for knitr::spin(...) converstion.

---
+++ R/model_small_sample.R
@@ -0,0 +1,161 @@
+#
+#  Utils - Small Sample Selection
+#
+# 
+
+
+#' @title Small Sample Variable Selection helper functions
+#' 
+#' @description Wrapper-based variable selection makes use of two functions, one
+#' for model training and one for model evaluation. This roots in the assumption of
+#' using the split set method for measuring generalization performance on true hold-out
+#' data. However, when you have a very small sample set this might seem problematic.
+#' 
+#' An way out then could be to estimate generalization performance using **cross-validation**
+#' or the **bootstrap**. In both approaches, the training function must be called on
+#' repeated subsamples (folds) such that the error can be estimated by combining 
+#' predictions from the excluded samples across all folds.
+#' 
+#' Here we'll provide two function factory methods, ie functions that take a training
+#' or a validation function and wrap it into one of these algorithms such that the
+#' call to the wrapper functions keeps the same interface. 
+#' The idea will be to replace fn_train by a dummy method that just returns a 
+#' dummy model. The fn_eval parameter will then consist of a function that incorporates
+#' the right fn_train function and applies it within the estimation proceedure.
+#' 
+#' \describe{
+#' \item{fn_train_dummy}{Dummy train function that just returns and class "DummyModel" object.}
+#' \item{ff_fn_eval_cross_validation}{Function generator (ff_) function that takes an fn_train and fn_eval function 
+#' together with n_folds parameter, and returns a function that calls fn_train and fn_eval to return a n_fold
+#' cross-validated estimate.}
+#' }
+#' 
+#' @name model_small_sample
+NULL
+
+#' @rdname model_small_sample
+#' @export
+fn_train_dummy <- function( dat, resp, selection, ... ) {
+  ret <- "Small sample Dummy model"
+  class(ret) <- "DummyModel"
+  return( ret )
+}
+
+
+#' @rdname model_small_sample
+#' @export
+ff_fn_eval_cross_validation <- function( arg_fn_train = NULL, arg_fn_eval = NULL, arg_n_folds = 10L ) {
+  stopifnot( !is.null(arg_fn_train) & !is.null(arg_fn_eval) )
+  stopifnot( !is.function(arg_fn_train) )
+  stopifnot( !is.function(arg_fn_eval) )
+  stopifnot( is.integer(arg_n_folds) & (1 < arg_n_folds) )
+  
+  fu <- local({
+    # Definition of constructed fn_eval 
+    function( dat, resp, selection, mod, ... ) {
+      if( is.null(mod) ) return( NA )
+      if( "DummyModel"!=class(mod) ) return( NA )
+      ret <- NA
+      
+      #
+      # Reference:
+      # Larry Wasssermann, "All of non-parametric statistics", Wiley, 2008
+      # Chapter 3.1, "The Jackknife", p.27, equation 3.1
+      #
+      
+      # 1) Estimate Tn
+      mod <- fn_train( dat, resp, selection, ... )
+      Tn <- fn_eval( dat, resp, selection, mod, ... )
+      
+      # 2) Estimate bias theta of Tn via jackknife / cross-validation
+      # Perform n_fold Cross-Validation here. Note: to avoid over-fitting to the
+      # folder, each time this function is called a new assignment to folds is
+      # sampled. This may seem less robust but prevents variable selection to favor
+      # a specific fixed fold configuration.
+      n <- nrow(dat)
+      ds <- rep_len( x = 1:n_folds, length.out = n )
+      ds <- ds[ order( runif( length(ds) ) ) ]
+      theta <- rep_len( NA, length.out = length(ds) )
+      for( k in 1:n_folds ) {
+        idx <- which( k == ds )
+        cvmod <- fn_train( dat[-idx,], resp, selection, ... )
+        cvevl <- fn_eval(  dat[ idx,], resp, selection, cvmod, ...  )
+        theta[idx] <- cvevl
+      }
+      # 3) Adjust estimate Tn by bias theta
+      bjack <- (n-1) * ( mean( theta, na.rm=TRUE ) - Tn )
+      Tjack <- Tn - bjack
+      
+      ret <- Tjack 
+      return( ret )
+    } # END(function)
+  }, envir = list( # Wrap function factory arguments into local environment
+                   fn_train = arg_fn_train, 
+                   fn_eval = arg_fn_eval, 
+                   n_folds = arg_n_folds 
+                   # END( envir argument )
+                   ) )
+  return( fu ) # Return constructed function
+}
+
+
+#' @rdname model_small_sample
+#' @export
+ff_fn_eval_bootstrap <- function( arg_fn_train = NULL, arg_fn_eval = NULL, arg_n_boots = 25L ) {
+  stopifnot( !is.null(arg_fn_train) & !is.null(arg_fn_eval) )
+  stopifnot( !is.function(arg_fn_train) )
+  stopifnot( !is.function(arg_fn_eval) )
+  stopifnot( is.integer(arg_n_folds) & (1 < arg_n_folds) )
+  
+  fu <- local({
+    # Definition of constructed fn_eval 
+    function( dat, resp, selection, mod, ... ) {
+      if( is.null(mod) ) return( NA )
+      if( "DummyModel"!=class(mod) ) return( NA )
+      ret <- NA
+      
+      #
+      # From Efron and Tibshirani, "An introduction to the bootstrap", Chapman & Hall/CRC, 1993
+      # Ch. 17 "Cross-validation and other estimates of prediction error", p. 237ff
+      # Ch. 17.6-7 Bootstrap estimates of prediction error / The .632 bootstrap estimator, p. 247f
+      #
+      didx <- 1:nrow(dat)
+      bt <- boot::boot( data = dat,
+                        statistic = funcion( dd,ii ) {
+                          mm  <- fn_train( dd[ii,], resp, selection, ... )
+                          vv  <- fn_eval( dd, resp, selection, mm, ... )
+                          vvb <- fn_eval( dd[ii,], resp, selection, mm, ... )
+                          jj  <- setdiff( didx, ii )
+                          vvo <- NA
+                          err0 <- NA
+                          if( 0 < length(jj) ) {
+                            vvo <- fn_eval( dd[jj,], resp, selection, mm, ... )  
+                            err0 <- vvo / length(jj)
+                          }
+                          
+                          st  <- c( bte_pred_error = vv,  # Bootstrap estimate of prediction error
+                                    apparent_error = vvb, # Apparent error
+                                    optimism = vv - vvb,  # Optimism
+                                    oobs_error = vvo,     # Out-of-bag sample error 
+                                    epsilon0   = err0     # Averaged OOB sample error (epsilon0)
+                                    )
+                          return( st )
+                        }, 
+                        R = n_boots,
+                        sim = "ordinary",
+                        stype = "i" )
+      # bt_ci <- lapply( 1:length(bt$t0), function(i) as.data.frame( boot::norm.ci( bt, index = as.integer(i) ) ) ) 
+      # ret <- mean( bt$t[,"bte_pred_error"], na.rm=TRUE )
+      ret <- mean( bt$t[,"bte_pred_error"], na.rm=TRUE ) + mean( bt$t[,"optimism"], na.rm=TRUE ) # Go with the optimism corrected bootstrap prediction error for now
+      # ret <- 0.368 * mean( bt$t[,"apparent_error"], na.rm=TRUE ) + 0.632 * mean( bt$t[,"oobs_error"], na.rm=TRUE ) # TODO Check again!
+      return( ret )
+    } # END(function)
+  }, envir = list( # Wrap function factory arguments into local environment
+    fn_train = arg_fn_train, 
+    fn_eval = arg_fn_eval, 
+    n_boots = arg_n_boots 
+    # END( envir argument )
+  ) )
+  return( fu ) # Return constructed function
+}
+  
--- main_fmi_varsel.R
+++ main_fmi_varsel.R
@@ -46,9 +46,6 @@ df_sel <- df_sel %>%
   mutate( A_IDD = ("YES"==A_IDD) )
 df_sel %>% summary
 
-resp1 <- "Surv( PFSINV_AVAL, PFSINV_EVENT )"
-# resp1 <- "A_IDD"
-
 g_set <- rep_len( c(1,1,1,2,3), length.out = nrow(df_sel) )
 g_set <- g_set[order( runif( length(g_set) ) )]
 table(g_set)
@@ -62,10 +59,17 @@ df_sel <- df_sel %>% mutate_at( names(which(lst_is_char)), function(x) x %>% as.
 df_test  <- df_sel[which(3==g_set),]
 df_eval  <- df_sel[which(2==g_set),]
 df_train <- df_sel[which(1==g_set),]
-
 df_trnevl <- bind_rows( df_train, df_eval )
 
 ds_prior <- prepare_splits( ds = 3L, dat=df_trnevl, resp = resp1, vars = lst_vars, fn_train = fn_train_cox, fn_eval = fn_eval_cox ) %>% as.matrix
+
+
+resp1 <- "Surv( PFSINV_AVAL, PFSINV_EVENT )"
+# resp1 <- "A_IDD"
+gmr_sel <- game_rank( dat = df_trnevl, resp = resp1, vars = lst_vars, fn_train = fn_train_cox, fn_eval = fn_eval_cox, m = 7L, team_size = 20L, rounds = 25L, maximize = FALSE, u = 365 )
+gmr_sel
+save( file_var_rds, lst_keys, lst_resp, gmr_sel, df_sel, df_test, df_train, df_eval, df_trnevl, lst_vars, resp1, file = "~/GameRank/out/20211103_gmr_result_cox.Rdata" )
+
 
 # rnd_sel <- random_selection( dat = df_trnevl, resp = resp1, vars = lst_vars, fn_train = fn_train_cox, fn_eval = fn_eval_cox, m = 7L, ds = ds_prior, nevals = 100L, maximize = FALSE, u = 365 )
 # rnd_sel
@@ -82,22 +86,6 @@ ds_prior <- prepare_splits( ds = 3L, dat=df_trnevl, resp = resp1, vars = lst_var
 # lrs_sel <- lrsearch( dat = df_trnevl, resp = resp1, vars = lst_vars[1:10], fn_train = fn_train_cox, fn_eval = fn_eval_cox, m = 3L, L = 3L, R = 2L, ds = ds_prior, maximize = FALSE, u = 365 )
 # lrs_sel
 
-gmr_sel <- game_rank( dat = df_trnevl, resp = resp1, vars = lst_vars, fn_train = fn_train_cox, fn_eval = fn_eval_cox, m = 7L, team_size = 20L, rounds = 25L, maximize = FALSE, u = 365 )
-gmr_sel
-
-vs = gmr_sel$solution
-HH = gmr_sel$inv_hessian
-
-mmp <- gmr_sel$match_matrix
-mmp <- ifelse( +1L==mmp, 1L, 0L )
-mmn <- gmr_sel$match_matrix
-mmn <- ifelse( -1L==mmn, 1L, 0L )
-mm <- rbind( mmp, mmn )
-
-tt <- estimate_T_matches(  mm, vs, HH )
-tt
-
-
 #
 # To Do List:
 #  - Add Oscillating Search Algorithm for Feature Selection
@@ -106,5 +94,4 @@ tt
 # sffs_sel <- sffs( dat = df_trnevl, resp = resp1, vars = lst_vars[1:10], fn_train = fn_train_cox, fn_eval = fn_eval_cox, m = 3L, ds = ds_prior, maximize = FALSE, u = 365 )
 # sffs_sel
 #
-
 
